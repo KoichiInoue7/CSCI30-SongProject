01edc29bc120e48f6a2f17c41fb8cad57fac15c3
Coding Rooms <git@codingrooms.io>
2026-03-18 15:39:23 +0000

    Finished 2.1 Lab

--- Playlist.cpp
+++ Playlist.cpp
@@ -41,12 +41,25 @@ void Playlist :: playAll()
     
 }
 
-
 void Playlist :: swapSongs(int index1, int index2)
 {
+    if(m_count < 2)
+    {
+        throw logic_error("LOGIC ERROR: Playlist must contain at least 2 items.\n");
+    }
+    else if((index1 < 0 || index1 >= m_count) ||(index2 < 0 ||index2 >= m_count))
+    {
+
+        throw std::range_error(
+        "RANGE ERROR: Invalid swap values <" + std::to_string(index1) + ">, <" + std::to_string(index2) + ">\n");
+    }
+
+    else
+    {
     AudioStream *temp = m_list[index1];
     m_list[index1] = m_list[index2];
     m_list[index2] = temp;
+    }
 }
 
 void Playlist ::showPlaylist()
--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #include "Playlist.h"
 #include "Song.h"
 #include "Podcast.h"
+#include <iostream>
 
-int main() {
+
+using namespace std;
+
+int main() 
+{
+try{
     Playlist myPlaylist;
 
-    // Direct heap allocation passed to the Playlist
-    myPlaylist.addStream(new Song("Starman", "David Bowie", 250, "Ziggy Stardust"));
-    myPlaylist.addStream(new Podcast("The Daily", "Michael Barbaro", 1800, 1024, "NYT Staff"));
-    myPlaylist.addStream(new Song("Blinding Lights", "The Weeknd", 200, "After Hours"));
+    // NOTE: WHEN YOU SUBMIT FOR TESTING, USE THE SAME STREAM DATA AS BELOW.
+    //   When you are testing the logic_error you will need to temporarily modify the code below so that
+    //   it does not add any streams to the playlist 
 
-    myPlaylist.swapSongs(0,1);
+    myPlaylist.addStream(new Song("Starman", "David Bowie", 250, "Ziggy Stardust"));
+    myPlaylist.addStream(new Song("Bohemian Rhapsody", "Queen", 354, "A Night at the Opera"));
+    myPlaylist.addStream(new Song("Hotel California", "Eagles", 391, "Hotel California"));
+    myPlaylist.addStream(new Song("Blinding Lights", "The Weeknd", 200, "After Hours"));
+    myPlaylist.addStream(new Podcast("The Daily", "Michael Barbaro", 1800, 1024, "NYT Staff"));
+    myPlaylist.addStream(new Podcast("Tech Overload", "Arun Maini", 1200, 45, "Tim Cook"));
+
+    int idx1, idx2;
+
     myPlaylist.showPlaylist();
 
-    myPlaylist.playAll();
+    cout << "\nEnter two indices to swap: ";
+    cin >> idx1 >> idx2;
 
-    return 0; // Playlist destructor handles memory cleanup
+    cout << "\nAttempting to swap index " << idx1 << " and " << idx2 << "..." << endl;
+    myPlaylist.swapSongs(idx1, idx2); 
+        
+    myPlaylist.showPlaylist();
+    }
+
+    catch(const logic_error &le)
+    {
+        cout << le.what() <<endl;
+    }
+
+    catch(const range_error &re)
+    {
+        cout <<re.what() <<endl;
+    }
+
+    return 0; 
 }
